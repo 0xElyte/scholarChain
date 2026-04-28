ad24f95a5522add2e6ba02de5177cfa970eae510
Feyisara2108 <ayokomi2108@gmail.com>
2026-04-28 03:09:57 +0100

    chore(GrantPool): remove draft to rebuild

---
+++ contract/src/Interfaces/IGrantPool.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IGrantPool {
+    function donate(uint256 amount) external;
+    function topUpPool(uint256 amount) external;
+    function editCriteria(bytes32 newCID) external;
+    function addSigner(address signer) external;
+    function cancelPool() external;
+    function submitProposal(bytes32 docCID, address payoutAddr) external;
+    function vote(address benefactor, bool approve) external;
+    function enterDistributionPhase() external;
+    function claimGrant() external;
+    function claimRefund() external;
+    function currentState() external view returns (string memory);
+    function getSigners() external view returns (address[] memory);
+    function getWinners() external view returns (address[] memory);
+    function getDonors() external view returns (address[] memory);
+    function quorumThreshold() external view returns (uint256);
+    function getApprovalCount(address benefactor) external view returns (uint256);
+    function hasVoted(address signer, address benefactor) external view returns (bool);
+    function totalDeposited() external view returns (uint256);
+    function distributionAmount() external view returns (uint256);
+    function isWinner(address) external view returns (bool);
+    function hasClaimed(address) external view returns (bool);
+    function isCancelled() external view returns (bool);
+    function isSigner(address) external view returns (bool);
+    function treasury() external view returns (address);
+    function creator() external view returns (address);
+}
